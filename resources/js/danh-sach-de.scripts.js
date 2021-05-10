441445d6a5b33af63178eb1cda717e21f6d2d34d
99-de-thi-thu <83701086+99-de-thi-thu@users.noreply.github.com>
2021-05-10 17:20:29 +0700

    Update danh-sach-de.scripts.js

--- resources/js/danh-sach-de.scripts.js
+++ resources/js/danh-sach-de.scripts.js
@@ -1,11 +1,18 @@
-function pagination(x, data) {
-    document.getElementById('position_1').href = data[x - 1][0]
-    document.getElementById('position_2').href = data[x - 1][1]
-    document.getElementById('position_3').href = data[x - 1][2]
-    document.getElementById('position_4').href = data[x - 1][3]
-    document.getElementById('position_5').href = data[x - 1][4]
-    document.getElementById('position_6').href = data[x - 1][5]
-    document.getElementById('position_7').href = data[x - 1][6]
-    document.getElementById('position_8').href = data[x - 1][7]
-    document.getElementById('position_9').href = data[x - 1][8]
+function pagination(trang_chuyen_den, data) {
+    n = data[length]
+    if (trang_chuyen_den + 4 < n) {
+        document.getElementById('position_1').href = data[parseInt(trang_chuyen_den) - 1][0]
+        document.getElementById('position_2').href = data[parseInt(trang_chuyen_den)][0]
+        document.getElementById('position_3').href = data[parseInt(trang_chuyen_den) + 1][0]
+        document.getElementById('position_4').href = data[parseInt(trang_chuyen_den) + 2][0]
+        document.getElementById('position_5').href = data[parseInt(trang_chuyen_den) + 3][0]
+        document.getElementById('position_6').href = data[parseInt(trang_chuyen_den) + 4][0]
+    } else {
+        document.getElementById('position_1').href = data[n - 6][0]
+        document.getElementById('position_2').href = data[n - 5][0]
+        document.getElementById('position_3').href = data[n - 4][0]
+        document.getElementById('position_4').href = data[n - 3][0]
+        document.getElementById('position_5').href = data[n - 2][0]
+        document.getElementById('position_6').href = data[n - 1][0]
+    }
 }
